58cfb1a23c430f8d3cf426ee2b28c3e67ef8dbfa
Sean Zellmer <sean@lejeunerenard.com>
2017-05-03 22:43:31 -0500

    refact: Separate rotationMatrix into module

--- frag.glsl
+++ frag.glsl
@@ -135,24 +135,7 @@ mat3 globalRot = mat3(
   0.0, 1.0,  0.0,
   gRs, 0.0,  gRc);
 
-// --------------------------------------------------------
-// http://www.neilmendoza.com/glsl-rotation-about-an-arbitrary-axis/
-// --------------------------------------------------------
-
-mat3 rotationMatrix(vec3 axis, float angle)
-{
-    axis = normalize(axis);
-    float s = sin(angle);
-    float c = cos(angle);
-    float oc = 1.0 - c;
-
-    return mat3(
-        oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,
-        oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,
-        oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c
-    );
-}
-
+#pragma glslify: rotationMatrix = require(./rotation-matrix3)
 #pragma glslify: dodeca = require(./dodec)
 
 vec3 quartz (in vec3 p, in float angle) {
