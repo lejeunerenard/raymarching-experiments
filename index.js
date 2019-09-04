52f1fdd7d034f4e806222b15c4ef937687aaa480
Sean Zellmer <sean@lejeunerenard.com>
2019-09-03 22:54:23 -0500

    feat: Create an offset grid which changes between circles & squares
    ig post 2019-09-03

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ if (capturing) {
   capturer = new CCapture({
     format: 'jpg',
     framerate: fr,
-    name: 'lost-in-the-flow-render1',
+    name: 'subtle-render1',
     autoSaveTime: 5,
     quality: 98,
     startTime: captureTime,
