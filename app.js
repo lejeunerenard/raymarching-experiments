e53d0d31e9ffa525b13cb496001383ab889d5f73
Sean Zellmer <sean@lejeunerenard.com>
2023-12-01 01:55:17 -0500

    feat: Crop w/ multiple shapes using smooth max
    ig 2023-11-30

--- app.js
+++ app.js
@@ -78,8 +78,8 @@ export default class App {
     this.rot2angle = preset.rot2angle || [0, 0, 0]
     this.cameraAngles = preset.cameraAngles || [0, 0, 0]
 
-    this.angle1C = 1.3312
-    this.angle2C = 0.118
+    this.angle1C = 0.6695
+    this.angle2C = 1.9694
     this.angle3C = 0.89
 
     // this.setupAnimation(preset)
--- frag.glsl
+++ frag.glsl
@@ -7,7 +7,7 @@
 // #define debugMapCalls
 // #define debugMapMaxed
 // #define SS 2
-#define ORTHO 1
+// #define ORTHO 1
 // #define NO_MATERIALS 1
 // #define DOF 1
 
@@ -1946,6 +1946,32 @@ vec3 map (in vec3 p, in float dT, in float universe) {
   vec3 b = vec3(dodecahedral(q, 52., r), 0, 0);
   d = dMin(d, b);
 
+  wQ = p;
+  wQ *= rotationMatrix(vec3(0, 1, 0), -localCosT );
+  wQ.x *= -1.;
+  wQ += 0.100000 * warpScale * cos( 2.182 * warpFrequency * componentShift(wQ) + distortT + warpPhase);
+  wQ += 0.050000 * warpScale * cos( 5.732 * warpFrequency * componentShift(wQ) + distortT + warpPhase);
+  warpPhase += warpPhaseAmp * componentShift(wQ);
+  wQ.xyz = twist(wQ.xzy, 1. * wQ.z + 0.4 * PI * cos(localCosT + 0.9 * wQ.z));
+  wQ += 0.025000 * warpScale * cos( 9.123 * warpFrequency * componentShift(wQ) + distortT + warpPhase);
+  wQ += 0.012500 * warpScale * cos(13.923 * warpFrequency * componentShift(wQ) + distortT + warpPhase);
+  warpPhase += warpPhaseAmp * componentShift(wQ);
+  wQ.xyz = twist(wQ.xzy, 0.35 * wQ.x + 0.305 * sin(localCosT + wQ.x));
+  wQ += 0.006250 * warpScale * cos(17.369 * warpFrequency * componentShift(wQ) + distortT + warpPhase);
+  wQ += 0.003125 * warpScale * cos(19.937 * warpFrequency * componentShift(wQ) + distortT + warpPhase);
+  warpPhase += warpPhaseAmp * componentShift(wQ);
+  wQ += 0.001125 * warpScale * cos(23.937 * warpFrequency * componentShift(wQ) + distortT + warpPhase);
+
+  // Commit warp
+  q = wQ.xyz;
+  mPos = q;
+
+  b = vec3(icosahedral(q, 52., r), 0, 0);
+  d = dSMax(d, b, 0.02 * r);
+
+  b = vec3(sdBox(p, vec3(r)), 0, 0);
+  d = dSMax(d, b, 0.02 * r);
+
   // d.x -= 0.007 * cellular(2. * q);
 
   // // Fractal Scale compensation
@@ -2095,7 +2121,7 @@ vec3 textures (in vec3 rd) {
   // dI *= 0.3;
 
   // -- Colors --
-  color = 0.5 + 0.5 * cos( TWO_PI * ( vec3(1) * dI + vec3(0, 0.33, 0.67) ) );
+  color = 0.5 + 0.5 * cos( TWO_PI * ( vec3(1) * dI + vec3(0, 0.1, 0.3) ) );
   // color = mix(#FF0000, #00FFFF, 0.5 + 0.5 * sin(TWO_PI * (dI)));
 
   // // - Rotated Components -
